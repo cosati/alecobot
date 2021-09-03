a00f435cfb2d6e209df751976d2f098198a04504
cosati <cosati.alexandre@gmail.com>
2021-09-03 18:54:19 -0300

    Sending data to arduino

--- camera.py
+++ camera.py
@@ -81,7 +81,7 @@ def arduino_job():
                     rgbLeds = arr[5]
                 except IndexError:
                     pass
-                ser.write(b'' + bytes(dataSend['sliderr']) + b';' + bytes(dataSend['sliderg']) + b';' + bytes(dataSend['sliderb']) + b'\n')
+            ser.write(bytes(dataSend['sliderr']) + b';' + bytes(dataSend['sliderg']) + b';' + bytes(dataSend['sliderb']) + b'\n')
                 # print(line, file=sys.stdout)                
                 # time.sleep(0.5)
 
